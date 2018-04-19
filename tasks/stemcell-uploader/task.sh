047813e5ad814ddf2a8554f9111503db34dddc18
ahocking <ahocking@pivotal.io>
2018-04-19 15:55:15 -0600

    use new stemcell uploader

--- tasks/stemcell-uploader/task.sh
+++ tasks/stemcell-uploader/task.sh
@@ -1,93 +1,27 @@
 #!/bin/bash
 
-set -eu
-
-if [[ -n "$NO_PROXY" ]]; then
-  echo "$OM_IP $OPSMAN_DOMAIN_OR_IP_ADDRESS" >> /etc/hosts
+if [[ $DEBUG == true ]]; then
+  set -ex
+else
+  set -e
 fi
 
-STEMCELL_VERSION=$(
-  cat ./pivnet-product/metadata.json |
-  jq --raw-output \
-    '
-    [
-      .Dependencies[]
-      | select(.Release.Product.Name | contains("Stemcells"))
-      | .Release.Version
-    ]
-    | map(split(".") | map(tonumber))
-    | transpose | transpose
-    | max // empty
-    | map(tostring)
-    | join(".")
-    '
-)
+SC_VERSION=`cat ./pivnet-product/metadata.json | jq -r '.Dependencies[] | select(.Release.Product.Name | contains("Stemcells")) | .Release.Version' | head -1`
 
-if [ -n "$STEMCELL_VERSION" ]; then
-  echo "Checking stemcell $STEMCELL_VERSION"
+STEMCELL_NAME=bosh-stemcell-$SC_VERSION-$IAAS_TYPE-esxi-ubuntu-trusty-go_agent.tgz
 
-  diagnostic_report=$(
-    om-linux \
-      --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
-      --client-id "${OPSMAN_CLIENT_ID}" \
-      --client-secret "${OPSMAN_CLIENT_SECRET}" \
-      --username "$OPS_MGR_USR" \
-      --password "$OPS_MGR_PWD" \
-      --skip-ssl-validation \
-      curl --silent --path "/api/v0/diagnostic_report"
-  )
+DIAGNOSTIC_REPORT=$(om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k curl -p /api/v0/diagnostic_report)
+STEMCELL_EXISTS=$(echo $DIAGNOSTIC_REPORT | jq -r --arg STEMCELL_NAME $STEMCELL_NAME '.stemcells | contains([$STEMCELL_NAME])')
 
-  stemcell=$(
-    echo $diagnostic_report |
-    jq \
-      --arg version "$STEMCELL_VERSION" \
-      --arg glob "$IAAS" \
-    '.stemcells[] | select(contains($version) and contains($glob))'
-  )
+if $STEMCELL_EXISTS ; then
+  echo "Stemcell already exists with Ops Manager, hence skipping this step"
+else
+  echo "Uploading stemcell $SC_VERSION"
 
-  if [[ -z "$stemcell" ]]; then
-    echo "Uploading stemcell $STEMCELL_VERSION"
 
-#    product_slug=$(
-#      jq --raw-output \
-#        '
-#        if any(.Dependencies[]; select(.Release.Product.Name | contains("Stemcells for PCF (Windows)"))) then
-#          "stemcells-windows-server"
-#        else
-#          "stemcells"
-#        end
-#        ' < pivnet-product/metadata.json
-#    )
-#
-#    pivnet-cli login --api-token="$PIVNET_API_TOKEN"
-#    pivnet-cli download-product-files -p "$product_slug" -r $STEMCELL_VERSION -g "*${IAAS}*" --accept-eula
+  SC_FILE_PATH=`find ./stemcells -name *.tgz`
 
-    SC_FILE_PATH=`find ./stemcells/ -name *.tgz`
+  om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k upload-stemcell -s $SC_FILE_PATH
 
-    if [ ! -f "$SC_FILE_PATH" ]; then
-      echo "Stemcell file not found!"
-      exit 1
-    fi
 
-    om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
-      --client-id "${OPSMAN_CLIENT_ID}" \
-      --client-secret "${OPSMAN_CLIENT_SECRET}" \
-      -u "$OPS_MGR_USR" \
-      -p "$OPS_MGR_PWD" \
-      -k \
-      upload-stemcell \
-      -s $SC_FILE_PATH
-  fi
 fi
-
-# Should the slug contain more than one product, pick only the first.
-FILE_PATH=`find ./pivnet-product -name *.pivotal | sort | head -1`
-om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
-  --client-id "${OPSMAN_CLIENT_ID}" \
-  --client-secret "${OPSMAN_CLIENT_SECRET}" \
-  -u "$OPS_MGR_USR" \
-  -p "$OPS_MGR_PWD" \
-  -k \
-  --request-timeout 3600 \
-  upload-product \
-  -p $FILE_PATH
