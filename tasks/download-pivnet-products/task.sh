b7a6c8509aa832406291b96b32fc7f359ecb4017
mling <sapient007@hotmail.com>
2018-11-24 08:28:39 -0500

    stubbed out functions not currently used

--- tasks/download-pivnet-products/task.sh
+++ tasks/download-pivnet-products/task.sh
@@ -11,23 +11,23 @@ function abort() {
   exit 1
 }
 
-function download_pivnet_stemcell() {
-  #downloads the stemcells associated with the pivnet product
-}
+# function download_pivnet_stemcell() {
+#   #downloads the stemcells associated with the pivnet product
+# }
 
 function download_pivnet_product() {
   #pivnet dlpf -p, --product-slug' and `-r, --release-version' -g Glob to match product name e.g. *aws*
   pivnet dlpf -p $1 -r $2 -g *${3}* -d $DOWNLOAD_PRODUCT_DIR --accept-eula
 }
 
-function clear_dirs() {
-  #clears the pivnet-product directory after each download
-}
+# function clear_dirs() {
+#   #clears the pivnet-product directory after each download
+# }
 
-function tar_pivnet_product() {
-  #tars up the pivnet-product directory and place it in output directory
+# function tar_pivnet_product() {
+#   #tars up the pivnet-product directory and place it in output directory
 
-}
+# }
 
 function main() {
   if [ -z "$API_TOKEN" ]; then abort "The required env var API_TOKEN was not set for pivnet"; fi
